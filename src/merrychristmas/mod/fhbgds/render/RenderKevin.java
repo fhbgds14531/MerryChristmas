eb71d522a95b97ca841f6453f839829aabad2c67
fhbgds14531 <rmshamannjam@gmail.com>
2013-08-13 15:30:22 -0700

    New commit

--- src/merrychristmas/mod/fhbgds/render/RenderKevin.java
+++ src/merrychristmas/mod/fhbgds/render/RenderKevin.java
@@ -1,48 +1,29 @@
 package merrychristmas.mod.fhbgds.render;
 
 import merrychristmas.mod.fhbgds.client.ModelKevin;
-import merrychristmas.mod.fhbgds.entity.EntityKevin;
 import merrychristmas.mod.fhbgds.lib.Reference;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.entity.RenderLiving;
-import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.passive.EntityCow;
-import net.minecraft.util.Icon;
 import net.minecraft.util.ResourceLocation;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
+@SideOnly(Side.CLIENT)
 public class RenderKevin extends RenderLiving {
-	public static ModelBase modelKevin = new ModelKevin();
-	private static final ResourceLocation field_110833_a = new ResourceLocation(Reference.MOD_ID + ":textures.entity.kevin.png");
+	private static final ResourceLocation field_110833_a = new ResourceLocation(Reference.MOD_ID, "textures/entity/kevin.png");
 
 	public RenderKevin(ModelBase model, float par2){
 		super(model, par2);
 	}
 	
-	@Override
-	protected void preRenderCallback(EntityLivingBase entity, float par2){
-		super.preRenderCallback(entity, par2);
-	}
-	
-	protected ResourceLocation func_110832_a(EntityLiving kevin){
+	protected ResourceLocation func_110832_a(EntityLiving entity){
         return field_110833_a;
     }
 
     protected ResourceLocation func_110775_a(Entity entity){
         return this.func_110832_a((EntityLiving)entity);
-    }
-
-
-	public void doRenderKevin(EntityKevin entity, double par2, double par4, double par6, float par8, float par9){
-        super.doRenderLiving(entity, par2, par4, par6, par8, par9);
-    }
-
-	@Override
-    public void doRenderLiving(EntityLiving par1EntityLiving, double par2, double par4, double par6, float par8, float par9){
-        this.doRenderKevin((EntityKevin)par1EntityLiving, par2, par4, par6, par8, par9);
     }
 }
